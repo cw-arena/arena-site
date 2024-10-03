e45b459aad3a44b347b9f7b51c4978caf2b9bff5
Forest Belton <65484+forestbelton@users.noreply.github.com>
2024-10-02 23:40:20 -0700

    Add site logo to navigation bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,27 +3,47 @@ import Button from "@mui/material/Button";
 import Toolbar from "@mui/material/Toolbar";
 import { Link, useLocation } from "wouter";
 import { matchesRoute, ROUTES } from "../routes";
+import LanIcon from "@mui/icons-material/Lan";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { default as MaterialLink } from "@mui/material/Link";
 
 const NavigationBar = () => {
   const [location] = useLocation();
 
   return (
     <AppBar position="static">
-      <Toolbar sx={{ gap: 5 }}>
-        {ROUTES.filter((route) => typeof route.name !== "undefined").map(
-          (route) => (
-            <Button
-              key={route.href}
-              component={Link}
-              href={route.href}
-              size="large"
-              sx={{ color: "white" }}
-              disabled={matchesRoute(route, location)}
-            >
-              {route.name}
-            </Button>
-          )
-        )}
+      <Toolbar>
+        <MaterialLink
+          display="inline-flex"
+          color="white"
+          component={Link}
+          href="/"
+          underline="none"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <LanIcon />
+          <Typography component="span" sx={{ ml: 2, mr: 7 }} fontWeight="bold">
+            CW Arena
+          </Typography>
+        </MaterialLink>
+        <Box display="flex" sx={{ gap: 2 }}>
+          {ROUTES.filter((route) => typeof route.name !== "undefined").map(
+            (route) => (
+              <Button
+                key={route.href}
+                component={Link}
+                href={route.href}
+                size="large"
+                sx={{ color: "white" }}
+                disabled={matchesRoute(route, location)}
+              >
+                {route.name}
+              </Button>
+            )
+          )}
+        </Box>
       </Toolbar>
     </AppBar>
   );
